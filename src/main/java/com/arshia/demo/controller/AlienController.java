02eb8d011b7bb50e03a40d75ec621196778ad009
Arshia Kaul <arshiakaul09@gmail.com>
2021-06-27 18:13:22 +0530

    Implement delete and update operation

--- src/main/java/com/arshia/demo/controller/AlienController.java
+++ src/main/java/com/arshia/demo/controller/AlienController.java
@@ -3,6 +3,8 @@ package com.arshia.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.arshia.demo.dao.AlienRepo;
 import com.arshia.demo.model.Alien;
@@ -21,6 +23,34 @@ public class AlienController {
 	@RequestMapping("/addAlien")
 	public String addAlien(Alien alien) {
 		
+		repo.save(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/getAlien")
+	public ModelAndView getAlien(@RequestParam int aid) {
+		
+		ModelAndView mv = new ModelAndView("showAlien.jsp");
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		mv.addObject(alien);
+		return mv;
+	}
+	
+	@RequestMapping("/deleteAlien")
+	public String deleteAlien(@RequestParam int aid) {
+		
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		repo.delete(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/updateAlien")
+	public String updateAlien(@RequestParam int aid, String aname, String tech) {
+		
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		alien.setAid(aid);
+		alien.setAname(aname);
+		alien.setTech(tech);
 		repo.save(alien);
 		return "home.jsp";
 	}
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -13,5 +13,23 @@
 		<input type="text" name="tech"><br>	
 		<input type="submit"><br>
 	</form>
+	
+	<form action="getAlien">
+		<input type="text" name="aid"><br>
+		<input type="submit"><br>
+	</form>
+	
+	<form action="deleteAlien">
+		<input type="text" name="aid"><br>
+		<input type="submit"><br>
+	</form>
+	
+	<form action="updateAlien">
+		<input type="text" name="aid"><br>
+		<input type="text" name="aname"><br>
+		<input type="text" name="tech"><br>
+		<input type="submit"><br>
+	</form>
+
 </body>
 </html>
