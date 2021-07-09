2ee6309f541f4bc4da3e8511eab1a3121d3db01a
Arshia Kaul <arshiakaul09@gmail.com>
2021-07-09 18:08:54 +0530

    Create PUT Request

--- src/main/java/com/arshia/demo/controller/AlienController.java
+++ src/main/java/com/arshia/demo/controller/AlienController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -58,5 +59,12 @@ public class AlienController {
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		
 		return repo.findById(aid);
+	}
+	
+	@PutMapping(path="/alien", consumes= {"application/json"})
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+		
+		repo.save(alien);
+		return alien;
 	}
 }
